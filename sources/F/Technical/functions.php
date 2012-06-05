f8e52db0ad39f84c97399bb00367be466feaa586
gbfs3157 <francoisschneider@neuf.fr>
2012-06-05 17:38:47 +0200

    + ajout d'une raccourci vers la methode de traduction

--- sources/F/Technical/functions.php
+++ sources/F/Technical/functions.php
@@ -12,11 +12,11 @@ function f_dbg($v, $exit = true, $dump = false)
 	$calledFrom = debug_backtrace();
 	$calledFrom = "\n=== DEBUG FROM ". substr($calledFrom[0]['file'], 1) .' (line ' . $calledFrom[0]['line'].")\n\n";
 	if (true === $dump) {
-		//header('Content-Type: text/html');
+		header('Content-Type: text/html');
 		echo $calledFrom;
 		var_dump($v);
 	} else {
-		//header('Content-Type: text/plain');
+		header('Content-Type: text/plain');
 		echo $calledFrom;
 		print_r($v);
 	}
@@ -26,6 +26,17 @@ function f_dbg($v, $exit = true, $dump = false)
 	if (true === $exit) {
 		exit(-1);
 	}
+}
+
+/**
+ * Raccourcie vers le service de traduction
+ *
+ * @param $key clef
+ * @param $args arguements si nécessaires
+ */
+function t($key, $args=null)
+{
+	return \F\Technical\Translate\Service::singleton()->translate($key, $args);
 }
 
 /**
